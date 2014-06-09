8f6e3d72d424ab7991862db2623c751777a05ccc
Yurand <polyukhovich@gmail.com>
2014-06-10 00:54:48 +0300

    first commit

---
+++ app/assets/javascripts/applicationjs.js
@@ -0,0 +1,27 @@
+/**
+ * Created with JetBrains RubyMine.
+ * User: polyukhovich
+ * Date: 02.06.14
+ * Time: 22:24
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+$(function(){
+
+$('#new_user_link').on('click', function(event){
+//    event.preventDefault();
+    console.log('link was clicked');
+});
+
+    $('#new_user_link').on('click', function(event){
+//    event.preventDefault();
+        console.log('link was clicked');
+    });
+
+    $("#create_user_form").on('submit', function (event){
+        console.log(this.);
+        event.preventDefault();
+//        event.stopPropagation();
+    });
+});
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ajaxtst::Application.config.secret_key_base = 'db47a1d5eb05690a1dce0a8a157227489ff15f596d95e7f31dafcbeb390734b9c917423994714655099bab5f30be3b2ae374c790a9de3ab92b10d8064cd854c6'
